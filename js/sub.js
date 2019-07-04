a5833f44621592799e7d115a7914ae7591ecf6e8
jeeweb <52396673+jeeweb@users.noreply.github.com>
2019-07-04 18:18:22 +0900

    Add files via upload

---
+++ js/sub.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    //네비게이션
+    var $gnb = $('#gnb > ul');
+    var dep1 = $('body').data('dep-one') -1;
+    var dep2 = $('body').data('dep-two') -1;
+    console.log(dep1, dep2);        
+
+    
+})
