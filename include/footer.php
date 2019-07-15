0e1a4c737efbd5034ac9e8c9027e6f22d71a79d8
jeeweb <52396673+jeeweb@users.noreply.github.com>
2019-07-15 09:36:54 +0900

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,14 @@
+<footer id="footer">
+    <ul class="sns">
+        <li><a href="#">카카오플러스 렛츠서프</a></li>
+        <li><a href="http://www.naver.com">네이버톡톡 렛츠서프</a></li>
+        <li><a href="http://www.instagram.com">인스타그램</a></li>
+    </ul>
+    <img src="../images/common/nav_logo.png" alt="렛츠서프" width="267" height="64">
+    <p><span class="blind">전화번호</span>070-1234-5678</p>
+    <p class="address"><span class="blind">주소</span>강원도 양양군 현남면 인구 중앙길 93 렛츠서프</p>
+    <div class="copyright">
+        <p>사업자등록번호 <span class="mhide">123-45-67890</span> <span class="line">통신판매업신고번호</span>   수상레저사업등록증</p>
+        <p>Copyright &copy; Lets Serf All Rights Reserved</p>
+    </div>
+</footer>   
